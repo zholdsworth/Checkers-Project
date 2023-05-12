49e656658d1f8d688eaa35e77eb65a0b944adb88
zholdsworth <zholdsworth@unomaha.edu>
2023-05-12 15:27:04 -0500

    Created constants.py and added some values

---
+++ checkers/constants.py
@@ -0,0 +1,11 @@
+import pygame
+
+WIDTH, HEIGHT = 800, 800
+ROWS, COLUMNS = 8, 8
+SQUARE_SIZE = WIDTH//COLUMNS
+
+#  RGB for game
+RED = (255, 0, 0)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BLUE = (0, 0, 255)
