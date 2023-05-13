4f710d9c05f36c8b939a189ad30759d9a6cd5bd2
zholdsworth <zholdsworth@unomaha.edu>
2023-05-12 19:01:01 -0500

    main.py updated to most recent ver.

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
-import pygame
+from checkers.constants import *
+from checkers.game import Game
 
-WIN = pygame.display.set_mod((WIDTH, HEIGHT)
+FPS = 60
+
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Checkers')
+
+
+def get_pos_mouse(pos):
+    x, y = pos
+    row = y // SQUARE_SIZE
+    column = x // SQUARE_SIZE
+    return row, column
+
+
+def main():
+    run = True
+    clock = pygame.time.Clock()
+    game = Game(WIN)
+
+    while run:
+        clock.tick(FPS)
+
+        if game.winner() != None:
+            print(game.winner())
+            run = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pos = pygame.mouse.get_pos()
+                row, column = get_pos_mouse(pos)
+
+                game.select(row, column)
+
+        game.update()
+
+    pygame.quit()
+
+
+main()
