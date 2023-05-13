562ce2a98e8e5d9d763352bf43dd26d293dcb88c
zholdsworth <zholdsworth@unomaha.edu>
2023-05-12 22:45:06 -0500

    First half of board.py with docstrings, type hinting and exception handling.

--- checkers/board.py
+++ checkers/board.py
@@ -3,65 +3,104 @@ from .pieces import Pieces
 
 
 class Board:
-    def __init__(self):
+    def __init__(self) -> None:
+        """
+        Initializes the Board class with the board state and player information.
+        """
         self.board = []
         self.red_left = self.white_left = 12
         self.red_kings = self.white_kings = 0
         self.create_board()
 
-    def squares(self, win):
-        win.fill(BLACK)
-        for row in range(ROWS):
-            for col in range(row % 2, ROWS, 2):
-                pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+    def squares(self, win: pygame.Surface) -> None:
+        """
+        Draws the red and black checkerboard squares on the screen.
+        :param: win (pygame.Surface): The surface to draw on.
+        """
+        try:
+            win.fill(BLACK)
+            for row in range(ROWS):
+                for col in range(row % 2, ROWS, 2):
+                    pygame.draw.rect(win, RED, (row*SQUARE_SIZE, col*SQUARE_SIZE, SQUARE_SIZE, SQUARE_SIZE))
+        except ValueError as e:
+            print(f"Error occurred in squares(): {e}")
 
-    def move(self, piece, row, column):
+    def move(self, piece, row: int, column: int) -> None:
+        """
+        Moves a checker piece on the board.
+        :param: piece (Pieces): The checker piece to move.
+        :param: row (int): The row to move the piece to.
+        :param: column (int): The column to move the piece to.
+        :return:
+        """
         # Swamps values by reversing
-        self.board[piece.row][piece.column], self.board[row][column] = \
-            self.board[row][column], self.board[piece.row][piece.column]
-        piece.move(row, column)
+        try:
+            self.board[piece.row][piece.column], self.board[row][column] = \
+                self.board[row][column], self.board[piece.row][piece.column]
+            piece.move(row, column)
 
-        if row == ROWS - 1 or row == 0:
-            piece.make_king()
-            if piece.color == WHITE:
-                self.white_kings += 1
-            else:
-                self.red_kings += 1
+            if row == ROWS - 1 or row == 0:
+                piece.make_king()
+                if piece.color == WHITE:
+                    self.white_kings += 1
+                else:
+                    self.red_kings += 1
+        except ValueError as e:
+            print(f"Error occurred in move(): {e}")
 
-    def get_piece(self, row, column):
+    def get_piece(self, row: int, column: int):
         return self.board[row][column]
 
-    def create_board(self):
-        for row in range(ROWS):
-            self.board.append([])
-            for col in range(COLUMNS):
-                # Determines where to place checker pieces
-                if col % 2 == ((row + 1) % 2):
-                    if row < 3:
-                        self.board[row].append(Pieces(row, col, WHITE))
-                    elif row > 4:
-                        self.board[row].append(Pieces(row, col, RED))
+    def create_board(self) -> None:
+        """
+        Creates the initial state of the checkerboard.
+        """
+        try:
+            for row in range(ROWS):
+                self.board.append([])
+                for col in range(COLUMNS):
+                    # Determines where to place checker pieces
+                    if col % 2 == ((row + 1) % 2):
+                        if row < 3:
+                            self.board[row].append(Pieces(row, col, WHITE))
+                        elif row > 4:
+                            self.board[row].append(Pieces(row, col, RED))
+                        else:
+                            self.board[row].append(0)
                     else:
                         self.board[row].append(0)
-                else:
-                    self.board[row].append(0)
+        except ValueError as e:
+            print(f"Error occurred in draw(): {e}")
 
-    def draw(self, win):
-        self.squares(win)
-        for row in range(ROWS):
-            for col in range(COLUMNS):
-                pieces = self.board[row][col]
-                if pieces != 0:
-                    pieces.draw(win)
+    def draw(self, win: pygame.Surface) -> None:
+        """
+        Draws the checkerboard and pieces on the screen.
+        :param: win (pygame.Surface): The surface to draw on.
+        """
+        try:
+            self.squares(win)
+            for row in range(ROWS):
+                for col in range(COLUMNS):
+                    pieces = self.board[row][col]
+                    if pieces != 0:
+                        pieces.draw(win)
+        except ValueError as e:
+            print(f"Error occurred in draw(): {e}")
 
-    def remove(self, pieces):
-        for piece in pieces:
-            self.board[piece.row][piece.column] = 0
-            if piece != 0:
-                if piece.color == RED:
-                    self.red_left -= 1
-                else:
-                    self.white_left -= 1
+    def remove(self, pieces) -> None:
+        """
+        Removes the checker pieces from the board
+        """
+        try:
+            for piece in pieces:
+                self.board[piece.row][piece.column] = 0
+                if piece != 0:
+                    if piece.color == RED:
+                        self.red_left -= 1
+                    else:
+                        self.white_left -= 1
+        except ValueError as e:
+            print(f"Error occurred in remove(): {e}")
 
     def winner(self):
         if self.red_left <= 0:
